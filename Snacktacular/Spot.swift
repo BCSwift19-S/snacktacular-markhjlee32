40ccb5adb47c17302d198566b66f93902dea1acb
Alice Liang <aliceliang011@gmail.com>
2019-03-31 21:15:00 -0400

    Initial Commit

---
+++ Snacktacular/Spot.swift
@@ -0,0 +1,34 @@
+//
+//  Spot.swift
+//  Snacktacular
+//
+//  Created by Alice Liang on 3/31/19.
+//  Copyright © 2019 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class Spot {
+    var name: String
+    var address: String
+    var coordinates: CLLocationCoordinate2D
+    var averageRating: Double
+    var numberOfReviews: Int
+    var postingUserID: String
+    var documentID: String
+    
+    init(name: String, address: String, coordinates: CLLocationCoordinate2D, averageRating: Double, numberOfReviews: Int, postingUserID: String, documentID: String) {
+        self.name = name
+        self.address = address
+        self.coordinates = coordinates
+        self.averageRating = averageRating
+        self.numberOfReviews = numberOfReviews
+        self.postingUserID = postingUserID
+        self.documentID = documentID
+    }
+    
+    convenience init() {
+        self.init(name: "", address: "", coordinates: CLLocationCoordinate2D(), averageRating: 0.0, numberOfReviews: 0, postingUserID: "", documentID: "")
+    }
+}
